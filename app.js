cb8d751e69479c8bbe4360492aad3529a169f6b0
nancycrojas <nanclarisaok@gmail.com>
2022-06-26 19:25:02 -0300

    Funciones - ejercicio 03

---
+++ app.js
@@ -0,0 +1,13 @@
+//multiplicar(a, b)
+//Crear una función multiplicar que tome como argumentos dos números y devuelva la multiplicación de ellos
+
+//multiplicar(2, 3) // 6
+//multiplicar(4, 0.5) // 2  
+
+const multiplicar = (a, b) => {
+//function multiplicar (a, b) {
+    return a * b;
+}
+
+multiplicar (2, 3);   
+multiplicar(4, 0.5);
